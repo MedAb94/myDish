7b31e06ab6450ae863267457444e157ec981a249
Medab <medab.vall@gmail.com>
2021-08-05 12:51:22 +0000

    deploy

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'https://my-dish-bktrans.herokuapp.com/';
 new Vue({
   router,
   store,
